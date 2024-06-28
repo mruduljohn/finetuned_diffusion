5488cda5d0a258a07d83550e716eda28519ae7a4
Mrudul John Mathews <67854062+mruduljohn@users.noreply.github.com>
2024-06-28 19:52:58 +0530

    Updated App.js

--- backend/App.js
+++ backend/App.js
@@ -36,9 +36,6 @@ function App() {
                 <button onClick={() => handleClick('8')}>8</button>
                 <button onClick={() => handleClick('9')}>9</button>
                 <button onClick={() => handleClick('/')}>/</button>
-                <button onClick={() => handleClick('4')}>4</button>
-                <button onClick={() => handleClick('5')}>5</button>
-                <button onClick={() => handleClick('6')}>6</button>
                 <button onClick={() => handleClick('*')}>*</button>
                 <button onClick={() => handleClick('1')}>1</button>
                 <button onClick={() => handleClick('2')}>2</button>
