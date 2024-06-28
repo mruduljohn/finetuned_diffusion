0b8880791c85d34de9c80d9fd7b61ba43aefefe7
Mrudul John Mathews <67854062+mruduljohn@users.noreply.github.com>
2024-06-28 19:53:39 +0530

    Removed some portion in App.js

--- backend/App.js
+++ backend/App.js
@@ -38,9 +38,6 @@ function App() {
                 <button onClick={() => handleClick('/')}>/</button>
                 <button onClick={() => handleClick('4')}>4</button>
                 <button onClick={() => handleClick('5')}>5</button>
-                <button onClick={() => handleClick('6')}>6</button>
-                <button onClick={() => handleClick('*')}>*</button>
-                <button onClick={() => handleClick('1')}>1</button>
                 <button onClick={() => handleClick('2')}>2</button>
                 <button onClick={() => handleClick('3')}>3</button>
                 <button onClick={() => handleClick('-')}>-</button>
